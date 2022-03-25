361514ee1c5ebdd47ccb258f805b0bce06259664
masa <dayoujianghong@gmail.com>
2022-03-25 13:07:46 +0900

    add pg

--- README.md
+++ README.md
@@ -6,4 +6,8 @@ This repo is used to support the CRT-600 class. Clone it and point the root of a
 You may wish to use the following VS Code extension to facilitate the creation of a quick web server: 
 https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer
 
+# Postgres
 
+1. `psql -U postgres -d postgres_db`
+
+2. `insert into users (name, age) values ('MY NAME', 20);`
--- server.js
+++ server.js
@@ -1,10 +1,24 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-var port = process.env.PORT || 5500;
+const { Pool } = require('pg')
+const pool = new Pool({
+  user: 'postgres',
+  host: 'postgres',
+  database: 'postgres_db',
+  password: 'password',
+  port: 5432,
+})
+
+const port = process.env.PORT || 5500;
 
 // Serve static files
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/users', async(req, res) => {
+  const { rows } = await pool.query('select * from users')
+  res.send(rows)
+});
 
 // Serve your app
 console.log('Served: http://localhost:' + port);
