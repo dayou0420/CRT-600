40aa72674237aad3d15bb16869108a4bb879c256
masa <dayoujianghong@gmail.com>
2022-03-28 20:54:19 +0900

    add public/Prac/fun20220328.js

---
+++ public/Prac/fun20220328.js
@@ -0,0 +1,85 @@
+debugger;
+
+/*
+同じ関数名だったら
+後からのものが実行される
+*/
+
+// function fn(...args) {
+//   console.log(args);
+//   const a = arguments[0];
+//   const b = arguments[1];
+//   console.log(arguments);
+//   console.log(a, b);
+//   return a;
+// }
+
+// const fn = function(a, b) {
+//   console.log(a, b);
+// }
+
+// function fn(a) {
+//   console.log(2);
+// }
+
+// fn(1);
+
+// let c = fn(1, undefined);
+// console.log(c);
+
+/*
+`関数`は`実行可能`な`オブジェクト`
+*/
+
+// function a() {
+//   console.log('Hello');
+// }
+
+// a.property = 0;
+// a.method = function() {
+//   console.log('Method');
+// }
+
+// a();
+// a.method();
+// console.log(a.property);
+
+/*
+コールバック関数
+他の関数に`引数として`
+渡される関数
+*/
+
+// function hello(name) {
+//   console.log(`Hello ${name}`);
+// }
+
+// function bye() {
+//   console.log('Bye');
+// }
+
+// function fn(cb) {
+//   cb('Tom');
+// }
+
+// fn(hello);
+// fn(bye);
+// fn(function(name) {
+//   console.log(`Hello ${name}`);
+// })
+
+// setTimeout(hello, 2000);
+
+/*
+`呼び出し元のオブジェクト`への
+参照を保持するキーワード
+*/
+
+const person = {
+  name: 'Tom',
+  hello: function() {
+    console.log(`Hello ${this.name}`)
+  }
+}
+
+person.hello();
