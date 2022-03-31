8f154d48db75ff33229367c7021f008652ff412c
masa <dayoujianghong@gmail.com>
2022-04-01 07:46:27 +0900

    add public/Prac/fun220220401.js

---
+++ public/Prac/fun220220401.js
@@ -0,0 +1,68 @@
+debugger;
+
+// let d = 0;
+
+// function fn() {
+//   let d = 1;
+//   const fn1 = new Function('a', 'b', 'return a * b * d');
+//   return fn1;
+// }
+
+// // const fn1 = new Function('a', 'b', 'return a + b * d');
+
+// const f = fn();
+
+// const result = f(1, 2);
+
+// console.log(result);
+
+// function fn2(a, b) {
+//   return a + b;
+// }
+
+// console.log(fn2 instanceof Function);
+
+// const obj = new function() {
+//   this.a = 0;
+// }
+
+// const fn3 = new Function('this.a = 0');
+
+// const obj3 = new fn3();
+
+// console.log(obj, fn3);
+
+/*
+ * プロトタイプチェーン
+ * プロトタイプの`多重形成`を
+ * プロトタイプチェーン
+ */
+
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  // this.hello = function() {
+  //   console.log(`OwnProperty: hello ${this.name}`);
+  // }
+}
+
+// Person.prototype.hello = function() {
+//   console.log(`Person: hello ${this.name}`);
+// }
+
+Object.prototype.hello = function() {
+  console.log(`Object: hello ${this.name}`);
+}
+
+const bob = new Person('Bob', 18);
+
+bob.hello();
+
+const result = bob.hasOwnProperty('name');
+
+console.log(result);
+
+// console.log('name' in bob);
+
+console.log('hello' in bob);
+
