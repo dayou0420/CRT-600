875108e3ae03ac609b939c5a62b70344bed1a2a8
masa <dayoujianghong@gmail.com>
2022-03-31 10:28:49 +0900

    add public/Prac/fun20220331.js

---
+++ public/Prac/fun20220331.js
@@ -0,0 +1,44 @@
+debugger
+
+// function a(name) {
+//     return `hello ${name}`;
+// }
+
+// const b = function(name) {
+//     return `hello ${name}`;
+// }
+
+// const b = (name, name2) => `hello ${name} ${name2}`;
+
+// const b2 = () => 'hello';
+
+// const b3 = _ => 'hello';
+
+// console.log(b('Tom', 'John'));
+// console.log(b2());
+// console.log(b3());
+
+// window.name = 'John';
+
+// const a = () => console.log(`Bye ${this.name}`);
+
+// const person = {
+//   name: 'Tom',
+// //   hello: function() {
+// //       console.log(`Hello ${this.name}`)
+// //   }
+//   hello() {
+//     console.log(`Hello ${this.name}`);
+//     const a = () => console.log(`Bye ${this.name}`);
+//     a();
+//   }
+// }
+
+// // person.hello();
+
+// function b() {
+//   const a = () => console.log(`Bye ${this.name}`);
+//   a();
+// }
+
+// b();
