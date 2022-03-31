61ef372885120837b72f33d083b6282a1585f4c1
masa <dayoujianghong@gmail.com>
2022-03-31 14:26:24 +0900

    edit public/Prac/fun92020220330.js

--- public/Prac/fun92020220330.js
+++ public/Prac/fun92020220330.js
@@ -7,7 +7,53 @@ const person = {
       console.log(greeting + ' ' + this.name);
       return greeting + ' ' + this.name;
   },
-  // hello1s: function() {
+  // hello1s() {
+  // }
+  /**
+   * 問題４：
+   * 1秒後に"hello Tom"
+   * と出力されるような、メソッドを
+   * personオブジェクトに追加してみてください。
+   * 
+   * 以下のように使用するものとします。
+   * `person.hello1s()` 
+   * -> 1秒後に"hello Tom"と出力
+   * 
+   * 3通りの方法で実装してみてください。
+   * １．bind
+   * ２．アロー関数
+   * ３．thisを一旦変数に代入
+   */
+}
+
+/**
+* 問題１：
+* 1秒後に"hello Tom"
+* と出力されるように、以下のコード
+* の記載を変更しましょう。
+*/
+// setTimeout(person.hello.bind(person, 'hello'), 1000);
+
+/**
+* 問題２：
+* alertで"hello Tom"
+* と出力されるように、
+* 以下のコードを変更してください。
+*/
+
+// alert(person.hello('hello'));
+
+/**
+* 問題３：
+* person.byeメソッドを１秒後に実行したかったので
+* bindを使って束縛してみましたが、コンソールには
+* "Bye"しか表示されませんでした。
+* "Bye Tom"とするためにはどうすればよいでしょうか？
+*/
+
+// setTimeout(person.bye.bind(person), 1000);
+
+// hello1s: function() {
   //   //   // console.log(`hello ${this.name}`);
   //   //   setTimeout(function() {
   //   //       // const hello = person.hello();
@@ -24,7 +70,7 @@ const person = {
   // hello1s: function() {
   //   setTimeout(this.hello.bind(this, 'hello'), 1000);
   // }
-  hello1s() {
+  // hello1s() {
     // const helloTom = this.hello.bind(this, 'hello');
     // setTimeout(helloTom, 1000);
     // setTimeout(this.hello.bind(this, 'hello'), 1000);
@@ -40,50 +86,21 @@ const person = {
     // setTimeout(function() {
     //   _this.hello('hello')
     // }, 1000);
-  }
-  /**
-   * 問題４：
-   * 1秒後に"hello Tom"
-   * と出力されるような、メソッドを
-   * personオブジェクトに追加してみてください。
-   * 
-   * 以下のように使用するものとします。
-   * `person.hello1s()` 
-   * -> 1秒後に"hello Tom"と出力
-   * 
-   * 3通りの方法で実装してみてください。
-   * １．bind
-   * ２．アロー関数
-   * ３．thisを一旦変数に代入
-   */
 
-
-  
-}
-
-person.hello1s();
-
-/**
-* 問題１：
-* 1秒後に"hello Tom"
-* と出力されるように、以下のコード
-* の記載を変更しましょう。
-*/
-// setTimeout(person.hello, 1000);
-
-/**
-* 問題２：
-* alertで"hello Tom"
-* と出力されるように、
-* 以下のコードを変更してください。
-*/
-// alert(person.hello);
-
-/**
-* 問題３：
-* person.byeメソッドを１秒後に実行したかったので
-* bindを使って束縛してみましたが、コンソールには
-* "Bye"しか表示されませんでした。
-* "Bye Tom"とするためにはどうすればよいでしょうか？
-*/
-// setTimeout(person.bye.bind(person), 1000);
+// hello1s: function() {
+  //   //   // console.log(`hello ${this.name}`);
+  //   //   setTimeout(function() {
+  //   //       // const hello = person.hello();
+  //   //       // const helloTom = person.hello.bind(person);
+  //   //       //const hello = person.hello.bind(null, 'Hello');
+  //   //       const hello = person.hello.bind(person, 'hello');
+  //   //       hello();
+  //   //   }, 1000)
+  //   //　setTimeout(this.hello.bind(this, 'hello'), 1000);
+  // }
+  // hello1s: () => {
+  //   setTimeout(person.hello.bind(person, 'hello'), 1000);
+  // },
+  // hello1s: function() {
+  //   setTimeout(this.hello.bind(this, 'hello'), 1000);
+  // }
