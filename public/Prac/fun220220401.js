e726b2a3e4f6187185f88be4671251ff71046303
masa <dayoujianghong@gmail.com>
2022-04-01 15:14:43 +0900

    edit public/Prac/fun220220401.js

--- public/Prac/fun220220401.js
+++ public/Prac/fun220220401.js
@@ -1,3 +1,5 @@
+'use strict';
+
 debugger;
 
 // let d = 0;
@@ -155,56 +157,158 @@ debugger;
 //   console.log(`さようなら ${this.name}`);
 // }
 
-class Person {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
+// class Person {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
 
-  hello() {
-    console.log(`hello ${this.name}`);
-  }
+//   hello() {
+//     console.log(`hello ${this.name}`);
+//   }
+// }
+
+// class Japanese extends Person {
+//   constructor(name, age, gender) {
+//     super(name, age);
+//     this.gender = gender;
+//   }
+
+//   hello() {
+//     super.hello();
+//     console.log(`こんにちは ${this.name}`);
+//   }
+
+//   bye() {
+//     console.log(`さようなら ${this.name}`);
+//   }
+// }
+
+// const taro = new Japanese('太郎', 23, 'Male');
+
+// taro.hello();
+
+// taro.bye();
+
+// const american = {
+//   name: 'Bob',
+//   hello() {
+//     console.log(`hello ${this.name}`);
+//   }
+// }
+
+// const bob = {
+//   name: 'Bob',
+//   hello() {
+//     super.hello();
+//   }
+//   // hello() {
+//   //   console.log(`hello ${this.name}`);
+//   // }
+// }
+
+// Object.setPrototypeOf(bob, american);
+
+// bob.hello();
+
+// const arry = new Array(1, 2, 3, 4);
+// console.log(arry);
+
+// arry[0];
+// arry.hasOwnProperty(0);
+
+// const a = new String('hello');
+// console.log(a.toUpperCase());
+
+// const a = 'hello'.toUpperCase();
+// console.log(a);
+
+// const b = new Number(100);
+// console.log(b.toExponential());
+
+/*
+ * シンボル
+ * プロパティーの重複を避けるために、
+ * `必ず一意の値`を返す関数
+ */
+
+// const s = Symbol('hello');
+// const s2 = Symbol('hello');
+
+// console.log(typeof s);
+
+// const str = new String('Tom');
+// console.log(str);
+
+// String.prototype[s] = function() {
+//   return `hello ${this}`;
+// }
+
+// const tom = 'Tom';
+// console.log(tom[s]());
+
+// const obj = { prop: 0 };
+
+// const obj = {};
+
+// Object.defineProperty(obj, 'prop', {
+//   configurable: true,
+//   value: 0,
+//   writable: true
+// })
+
+// delete obj.prop;
+
+// // obj.prop = 1;
+// console.log(obj.prop);
+
+// const descriptor = Object.getOwnPropertyDescriptor(obj, 'prop');
+
+// console.log(descriptor);
+
+function Person1(name, age) {
+  this._name = name;
+  this._age = age;
 }
 
-class Japanese extends Person {
-  constructor(name, age, gender) {
-    super(name, age);
-    this.gender = gender;
+Object.defineProperty(Person1.prototype, 'name', {
+  get: function() {
+    console.log('hello');
+    return this._name;
+  },
+  set: function(val) {
+    this._name = val;
+  }
+})
+
+const p1 = new Person1('Bob', 23);
+
+// p1.name = 'Tom';
+
+console.log(p1.name);
+
+class Person2 {
+
+  constructor() {
+    this._name = name;
+    this._age = age;
   }
 
-  hello() {
-    super.hello();
-    console.log(`こんにちは ${this.name}`);
+  get name() {
+    console.log('hello');
+    return this._name;
   }
 
-  bye() {
-    console.log(`さようなら ${this.name}`);
+  set name(val) {
+    this._name = val;
   }
+
+  static hello() {
+    console.log('hello');
+  }
+
 }
 
-const taro = new Japanese('太郎', 23, 'Male');
+const p2 = new Person2('Bob', 23);
 
-taro.hello();
-
-taro.bye();
-
-const american = {
-  name: 'Bob',
-  hello() {
-    console.log(`hello ${this.name}`);
-  }
-}
-
-const bob = {
-  name: 'Bob',
-  hello() {
-    super.hello();
-  }
-  // hello() {
-  //   console.log(`hello ${this.name}`);
-  // }
-}
-
-Object.setPrototypeOf(bob, american);
-
-bob.hello();
+Person2.hello();
